d96745198ae90458aac236a0640c517b30f21559
hannahhelix <hannahbemnetpais@gmail.com>
2024-05-20 15:33:50 -0500

    basic outline of homepage

--- back/obj/Debug/net8.0/back.AssemblyInfo.cs
+++ back/obj/Debug/net8.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+672159cf16b6378fd53f3e547adad7175fc8eb63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b669b4ea277a44d9661af5d616d064f79b9ec4")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ front/frontend/src/Homepage.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Button, Container, Row, Col, ProgressBar } from 'react-bootstrap';
+import "./App.css"
+import Budgets from './Budgets';
+import Savings from './Savings';
+
+function HomePage() {
+    const budgetGoals = [
+        { name: 'Food Budget', progress: 60 },
+        { name: 'Rent Budget', progress: 80 },
+        { name: 'Utilities Budget', progress: 50 },
+      ]; 
+
+  return (
+    <Router>
+        <Routes>
+        <Route path="/" element={
+            <Container className="summary-container">
+            <Row className="summary-section">
+                <Col xs={12}>
+                <h2>Weekly Summary</h2>
+                <ProgressBar now={75} className="progress-bar-custom" label="75%" />
+                </Col>
+            </Row>
+            <div className="horizontal-line"></div>
+            <Row>
+                <Col md={8}>
+                <h3><b>Quick Look</b></h3>
+                {budgetGoals.map((goal, index) => (
+                    <div key={index} className="mb-3">
+                    <h5>{goal.name}</h5>
+                    <ProgressBar 
+                        now={goal.progress} 
+                        className="progress-bar-custom" 
+                        label={`${goal.progress}%`} 
+                        style={{backgroundColor: 'var(--light-light-blue)'}}
+                    />
+                    </div>
+                ))}
+                </Col>
+                <Col md={4} className="d-flex justify-content-end">
+                <div className="buttons-container">
+                    <Link to="/budgets">
+                    <Button className="custom-button">Budgets</Button>
+                    </Link>
+                    <Link to="/savings">
+                    <Button className="custom-button">Savings</Button>
+                    </Link>
+                </div>
+                </Col>
+            </Row>
+            </Container>
+        } />
+        <Route path="/budgets" element={<Budgets />} />
+        <Route path="/savings" element={<Savings />} />
+        </Routes>
+    </Router>
+    );
+}
+
+export default HomePage;
