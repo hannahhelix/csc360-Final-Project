d91b38df0dea494fb9e8775a718f9f946374ead8
hannahhelix <hannahbemnetpais@gmail.com>
2024-05-20 15:34:18 -0500

    start of budgets page

---
+++ front/frontend/src/Budgets.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { ListGroup, Button, Form, Container, Row, Col, Tab, Nav, Dropdown } from 'react-bootstrap';
+import "./Budgets.css"
+
+function Budgets() {
+  return (
+    <h2>This is Budgets</h2>
+  );
+}
+
+export default Budgets;
