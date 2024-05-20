2f7dd4da675c2f2477d7a35e4dd9ef7337f9f13c
hannahhelix <hannahbemnetpais@gmail.com>
2024-05-20 15:34:06 -0500

    the start of the savings page

---
+++ front/frontend/src/Savings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { ListGroup, Button, Form, Container, Row, Col, Tab, Nav, Dropdown } from 'react-bootstrap';
+import "./Savings.css"
+
+function Savings() {
+  return (
+    <h2>This is Savings</h2>
+  );
+
+}
+
+export default Savings;
