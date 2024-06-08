881779a4e811de24dc44887a0d616da72c6188a5
hannahhelix <hannahbemnetpais@gmail.com>
2024-06-08 16:05:47 -0500

    working final

--- back/obj/Debug/net8.0/back.AssemblyInfo.cs
+++ back/obj/Debug/net8.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e77263e83baa13aab4a35dcfc5fafd00f880dc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a985f1faa43717d95aaf1204a57f66a746d09bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
