263be56bca7487c4f2058d5aace4fe065b03ebfa
hannahhelix <hannahbemnetpais@gmail.com>
2024-06-08 16:43:48 -0500

    adjusted db and login loading

--- back/obj/Debug/net8.0/back.AssemblyInfo.cs
+++ back/obj/Debug/net8.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a985f1faa43717d95aaf1204a57f66a746d09bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+881779a4e811de24dc44887a0d616da72c6188a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
